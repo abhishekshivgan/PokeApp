dab0e7df8b90fcecdd769aff17f5d5b13c21f64d
Abhishek <abhisheklitlost@gmail.com>
2023-06-26 15:39:08 +0530

    first commit

---
+++ src/Components/Main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Main() {
+    return (
+       <>
+            <div className="container">
+                <div className="left-content">
+
+                </div>
+
+                <div className="right-content">
+                    
+                </div>
+            </div>
+       </>
+    )
+}
+
+export default Main;
